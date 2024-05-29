f4af1d39323b5cd0778f1e974e111a3491099d49
Bhanu <k_redu@outlook.com>
2024-05-29 12:01:16 +0530

    add

--- shellscript/01-hello-world.sh
+++ shellscript/01-hello-world.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
 echo "Hello world, I am learning Aws DevOps"
+
+PERSON1=bhanu
+PERSON2=sreenu
+
+echo " $PERSON1 : Hi $PERSON2, Good Morning"
+echo " $PERSON2 : I am fine , How are you $PERSON1"
