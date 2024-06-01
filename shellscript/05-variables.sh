a85bb982c20746d56bc305dd3bd58cf90e9e740c
Bhanu <k_redu@outlook.com>
2024-06-01 08:08:25 +0530

    add

--- shellscript/05-variables.sh
+++ shellscript/05-variables.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-echo " Please enter your username :"
-read -s USERNAME
-echo "Username eneters is :$USERNAME"                                                   
+# echo " Please enter your username :"
+# read -s USERNAME
+# echo "Username eneters is :$USERNAME"                                                   
 
-echo " Please enter your password :"
-read -s PASSWORD
-echo "Username eneters is :$PASSWORD"
+# echo " Please enter your password :"
+# read -s PASSWORD
+# echo "Username eneters is :$PASSWORD"
+
+USERNAME=$1
+PASSWORD=$2
+echo "Please enter your username :$USERNAME"
+echo "Please enter your password: $PASSWORD"
