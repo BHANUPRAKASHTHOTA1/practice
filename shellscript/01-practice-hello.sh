e6aecdba84583c479ad24693285b301332027798
Bhanu <k_redu@outlook.com>
2024-06-01 07:38:42 +0530

    add

---
+++ shellscript/01-practice-hello.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "I am learning AWS DevOps, From join DevOps institute"
+PERSON1=BHANU
+PERSON2=Institute Trainer
+
+
+echo "$PERSON1 : I want to join Devops Course, What is the fees ?"
+echo "$PERSON2 : ok $PERSON1, the course fee is rs 20,000.."
+echo "$PERSON1 : Any student discount $PERSON2"
+echo "$PERSON2 : There may be a Course join discount $PERSON1"
+echo "$PERSON1 : what is the discount amount  ?"
+echo "$PERSON2 : $PERSON1, The discount amount is rs 2,000"
+echo "$PERSON1 : Then Please register my name in this course $PERSON2"
