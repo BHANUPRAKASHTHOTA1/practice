dae14391cf5a1f36642c7e713f40e0aab5a0d865
Bhanu <k_redu@outlook.com>
2024-06-01 07:55:30 +0530

    add

--- shellscript/03-variables.sh
+++ shellscript/03-variables.sh
@@ -12,3 +12,7 @@ echo "$PERSON2 : There may be a Course join discount $PERSON1"
 echo "$PERSON1 : what is the discount amount  ?"
 echo "$PERSON2 : $PERSON1, The discount amount is rs 2,000"
 echo "$PERSON1 : Then Please register my name in this course $PERSON2"
+
+
+
+# on the time of execution we need to pass the values at sh 03-variables.sh Bhanu anil
