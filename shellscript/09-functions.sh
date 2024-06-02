a24c5b5639a0f51dac5a6d7df74497acfbb6aada
Bhanu <k_redu@outlook.com>
2024-06-02 07:38:11 +0530

    add

---
+++ shellscript/09-functions.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+USERID=$(id -u)
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo "Installation .... FAIL"
+        exit 1
+
+    else
+        echo " Installation .... SUCCESS"
+
+    fi 
+
+}
+
+if [ $USERID -ne 0 ]
+then
+    echo "ERROR :: Please run the below command with sudo access"
+    exit 1
+else
+    echo "You are running on sudo access"
+
+fi 
+
+
+
+yum install mysql -y
+
+VALIDATE $? 
