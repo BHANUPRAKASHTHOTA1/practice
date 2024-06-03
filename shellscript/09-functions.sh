48b684872b338a529bd6cbdecdbc6fdaa7f9a257
Bhanu <k_redu@outlook.com>
2024-06-03 07:30:35 +0530

    add

--- shellscript/09-functions.sh
+++ shellscript/09-functions.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
-USERID=$(id -u)
+DATE=$(date +%F -%H-%M-%s)
+SCRIPT_NAME = $0
+LOG_FILE=\tmp\$SCRIPT_NAME-$DATE.log
+R=\e[31m
+G=\e[32m
+N=\e[0m
+
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo "$2 .... FAIL"
+        echo "$2 .... $R FAIL $N"
         exit 1
 
     else
-        echo "$2 .... SUCCESS"
+        echo "$2 .... $G SUCCESS $N"
 
     fi 
 
 }
+USERID=$(id -u)
 
 if [ $USERID -ne 0 ]
 then
