671e8849ec4b968136b4190efc89f01a3f280598
Bhanu <k_redu@outlook.com>
2024-06-03 08:03:04 +0530

    add

--- shellscript/07-condition.sh
+++ shellscript/07-condition.sh
@@ -5,11 +5,11 @@ echo "$SCRIPT_NAME"
 LOGFILE=/tmp/$SCRIPT_NAME-$DATE.log
 NUMBER=$1
 
-if [ $NUMBER -gt 10 ] &>>LOGFILE
+if [ $NUMBER -gt 10 ] 
 then
-    echo "$NUMBER is greater than 10"
+    echo "$NUMBER is greater than 10" &>>LOGFILE
 
 else
-    echo "$NUMBER is not greater than 10"
+    echo "$NUMBER is not greater than 10" &>>LOGFILE
 
 fi 
